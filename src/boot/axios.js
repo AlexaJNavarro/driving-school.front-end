ccc8583b8fc5629ac20f43d24e134a6f376620d5
AlexaJNavarro <ale2002.nn13@gmail.com>
2021-06-09 01:23:04 -0500

    update simulacro

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+
+const api = axios.create({baseURL:'http://localhost:8081/api/v1'})
+Vue.prototype.$api = api
+
+export { axios, api}
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="q-pa-md">
-    <div v-for="(d,index) in data" :key="index">
+    <div v-for="list in list_value" :key="list">
 
-      <span class="label bg-primary text-white">{{ d.question }}</span>
+      <span class="label bg-primary text-white">{{ data[list].question }}</span>
 
-      <div v-if="d.image != ''">
+      <div v-if="data[list].image != ''">
         <div class="col-4">
-          <q-img :src="d.image" :ratio="4/3"/>
+          <q-img :src="data[list].image" :ratio="4/3"/>
         </div>
       </div>
 
-      <div class="q-gutter-sm" v-for="option in 3" :key="option">
-        <q-radio v-model="d.value" :val="option" :label="d[option]" color="pink"/>
+      <!-- <div class="q-gutter-sm" v-for="option in 3" :key="option">
+        <q-radio v-model="data[list].value" :val="option" :label="data[list][option]" color="pink"/>
+      </div> -->
+      <div class="q-gutter-sm">
+        <q-radio v-model="data[list].response" :val="1" :label="data[list].option1" color="pink"/><br>
+        <q-radio v-model="data[list].response" :val="2" :label="data[list].option2" color="pink"/><br>
+        <q-radio v-model="data[list].response" :val="3" :label="data[list].option3" color="pink"/><br>
+        <q-radio v-model="data[list].response" :val="4" :label="data[list].option4" color="pink"/>
       </div>
 
     </div>
@@ -22,31 +28,35 @@
 </template>
 
 <script>
+import {api} from 'boot/axios'
 export default {
   name: 'PageIndex',
   data () {
     return {
-      data:[
-        {question:'pregunta 1',image:'https://www.nippon.com/es/ncommon/contents/japan-topics/560508/560508.jpg', 1: 'option1_valued', 2: 'option2f', 3: 'option3e',value:0},
-        {question:'pregunta 2',image:'', 1: 'option1a', 2: 'option2_value', 3: 'option3f',value:0},
-        {question:'pregunta 3',image:'', 1: 'option1b', 2: 'option2c', 3: 'option3_value',value:0}
-      ],
+      data:[],
       list_value:[]
     }
   },
   methods:{
     saludo(){
       console.log(this.data)
+    },
+    async getAll(){
+      const response = await api.get('/data/')
+      console.log(response.data)
+      this.data= response.data
     }
   },
   created(){
-    while (this.list_value.length < 40){
-      var value = Math.floor(Math.random() * 40)
+    while (this.list_value.length < 3){
+      var value = Math.floor(Math.random() * 3)
       if (!this.list_value.includes(value)){
         this.list_value.push(value);
       }
     }
-    console.log(this.list_value)
+
+    this.getAll()
+
   }
 }
 </script>
